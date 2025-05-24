99676ed99e4f943249cbbe0960aeba294f2f98d0
victor kamau <vickamworkpro@gmail.com>
2025-05-24 12:43:29 +0300

    initialize project

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tech Fair Evaluation</title>
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+    <main>
+        <h1> Welcome to the Tech Fair Evaluation</h1>
+
+        <ul><a href="login.php">Login</a></ul>
+        <ul><a href="signup.php">Signup</a></ul>
+        <ul><a href="admin.php">Admin panel</a></ul>
+        <ul><a href="scoreboard.php">View scoreboard </a></ul>
+    </main>
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>
